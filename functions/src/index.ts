f45db41c97d7552ac4f6227e7a17f796e9332a1c
arjun <arjun@nairsnet.com>
2021-07-30 02:41:51 -0400

    try to add some auth stuff. still prolly dont work yet.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import { executeSql } from "./mysql";
-import {app} from "./onRequest";
+import {app} from "./api";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -35,79 +35,79 @@ export const createUser = functions.auth.user().onCreate((user) => {
 // with promises but no, I don't want to use them. Simply because
 // I don't just trust libraries that easily
 // TODO figure out how to authenticate this
-export const getUser = functions.https.onRequest((req, res) => {
-    const sql = "SELECT * FROM User WHERE id=?";
-    const values = [req.body.uid];
-    executeSql(sql, values, (success, results, fields) => {
-        functions.logger.info("ENTIRE CREATE USER FUNCTION COMPLETED");
-        console.assert(results.length == 1);
-        res.send(results[0]);
-    });
-});
+// export const getUser = functions.https.onRequest((req, res) => {
+//     const sql = "SELECT * FROM User WHERE id=?";
+//     const values = [req.body.uid];
+//     executeSql(sql, values, (success, results, fields) => {
+//         functions.logger.info("ENTIRE CREATE USER FUNCTION COMPLETED");
+//         console.assert(results.length == 1);
+//         res.send(results[0]);
+//     });
+// });
 
-export const makePayment = functions.https.onCall(async (data) => {
-    const db = admin.firestore();
-    const message = data.message;
-    const amount = data.amount;
-    const senderUid = data.senderUid;
-    const recipientEmail = data.recipientEmail;
-    if (amount <= 0) {
-        console.log(amount);
-        throw new Error("Amount must be greater than 0");
-    }
-    const batch = db.batch();
-    const senderDoc = db.collection("User").doc(senderUid);
-    batch.update(senderDoc, {
-        "balance": admin.firestore.FieldValue.increment(-amount),
-    });
+// export const makePayment = functions.https.onCall(async (data) => {
+//     const db = admin.firestore();
+//     const message = data.message;
+//     const amount = data.amount;
+//     const senderUid = data.senderUid;
+//     const recipientEmail = data.recipientEmail;
+//     if (amount <= 0) {
+//         console.log(amount);
+//         throw new Error("Amount must be greater than 0");
+//     }
+//     const batch = db.batch();
+//     const senderDoc = db.collection("User").doc(senderUid);
+//     batch.update(senderDoc, {
+//         "balance": admin.firestore.FieldValue.increment(-amount),
+//     });
 
-    const query = await db.collection("User")
-        .where("email", "==", recipientEmail)
-        .get();
+//     const query = await db.collection("User")
+//         .where("email", "==", recipientEmail)
+//         .get();
 
-    if (query.empty) {
-        throw new Error("user with email \"" + recipientEmail + "\" does not exist");
-    } else if (query.docs.length > 1) {
-        throw new Error("too many users with email \"" + recipientEmail + "\"");
-    }
+//     if (query.empty) {
+//         throw new Error("user with email \"" + recipientEmail + "\" does not exist");
+//     } else if (query.docs.length > 1) {
+//         throw new Error("too many users with email \"" + recipientEmail + "\"");
+//     }
 
-    const recipientDoc = query.docs[0].ref;
-    const recipientData = query.docs[0].data();
-    const recipientUid = recipientData["uid"];
-    const recipientDisplayName = recipientData["displayName"];
+//     const recipientDoc = query.docs[0].ref;
+//     const recipientData = query.docs[0].data();
+//     const recipientUid = recipientData["uid"];
+//     const recipientDisplayName = recipientData["displayName"];
 
 
-    batch.update(recipientDoc, {
-        "balance": admin.firestore.FieldValue.increment(amount),
-    });
+//     batch.update(recipientDoc, {
+//         "balance": admin.firestore.FieldValue.increment(amount),
+//     });
 
-    const payment = db.collection("Payment").doc();
+//     const payment = db.collection("Payment").doc();
 
-    batch.set(payment, {
-        senderUid: senderUid,
-        recipientUid: recipientUid,
-        recipientDisplayName: recipientDisplayName,
-        amount: amount,
-        message: message,
-        timestamp: admin.firestore.FieldValue.serverTimestamp(),
-    });
+//     batch.set(payment, {
+//         senderUid: senderUid,
+//         recipientUid: recipientUid,
+//         recipientDisplayName: recipientDisplayName,
+//         amount: amount,
+//         message: message,
+//         timestamp: admin.firestore.FieldValue.serverTimestamp(),
+//     });
 
-    return batch.commit().then(() => {
-        return {
-            success: "true",
-        };
-    });
-    // const recipientDoc = db.collection("User").doc(senderUid);
+//     return batch.commit().then(() => {
+//         return {
+//             success: "true",
+//         };
+//     });
+//     // const recipientDoc = db.collection("User").doc(senderUid);
 
-    // batch.update(recipientDoc, {
-    //   "balance": FieldValue.increment(balance)
-    // });
+//     // batch.update(recipientDoc, {
+//     //   "balance": FieldValue.increment(balance)
+//     // });
 
-    // return {
-    //   success: "true",
-    // };
+//     // return {
+//     //   success: "true",
+//     // };
 
-    // return ["Apple", "Banana", "Cherry", "Date", "Fig", "Grapes"];
-});
+//     // return ["Apple", "Banana", "Cherry", "Date", "Fig", "Grapes"];
+// });
 
-export const onRequest = app;
+export const api = functions.https.onRequest(app);
